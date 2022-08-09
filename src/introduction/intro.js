a1b6c5e3a2b6d9797b22f08c3c9b7eb4d44167db
sikendrakumarverma <grdskverma2018@gmail.com>
2022-08-09 09:37:20 +0530

    assignment2

--- src/introduction/intro.js
+++ src/introduction/intro.js
@@ -3,8 +3,56 @@ const batchName = "Plutonium"
 let printName = function() {
     console.log('Bathc name is ', batchName)
 }
+// const movies=['suryawansam','three idiot','pk','nadiya ke par']
+// const moviesname=movies; 
+// console.log(moviesname);
+
+// const indexnum=5;
+// if(indexnum==1) {
+//     console.log(movies[1]);
+// } else if(indexnum==2) {
+//     console.log(movies[2]);
+// } else if(indexnum==3) {
+//     console.log(movies[3]);
+// } else if(indexnum==0) {
+//     console.log(movies[0]);
+// } else{
+//   console.log('Enter a valid index number')
+// }
+
+// const films=[ {
+//     id: 1,
+//     “name”: “The Shining”
+//    }, {
+//     “id”: 2,
+//     “name”: “Incendies”
+//    }, {
+//     “id”: 3,
+//     “name”: “Rang de Basanti”
+//    }, {
+//     “id”: 4,
+//     “name”: “Finding Nemo”
+//    }]
+//    console.log(films);
+
+//    const id=1
+//    if(id==1) {
+//     console.log(films[0]);
+//    } else if(id==2) {
+//     console.log(films[1]);
+//    } else if(id==3) {
+//     console.log(films[2]);
+//    } else if(id==4) {
+//     console.log(films[3]);
+//    } else {
+//     console.log("No movie exist in this id")
+//    }
 
 module.exports.name = batchName
 module.exports.printName = printName
+// module.exports.movies=moviesname
+// module.exports.films=films
+// module.exports.idvalue=id
+
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,14 +6,34 @@ router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
     logger.welcome()
-
+    abc.moviesname()
+    abc.films()
+    abc.idvalue()
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+router.get('/movies', function (req, res){
+    let movies = [ 'suryawansam', 'three idiot', 'pk', 'nadiya ke par' ]
+    res.send(movies)
 })
+router.get('/indexNumber', function (req, res){
+    let movies = [ 'suryawansam', 'three idiot', 'pk', 'nadiya ke par' ]
+    let indexNumber = 1
+    if(indexnum==1) {
+            console.log(movies[1]);
+        } else if(indexnum==2) {
+            console.log(movies[2]);
+        } else if(indexnum==3) {
+            console.log(movies[3]);
+        } else if(indexnum==0) {
+            console.log(movies[0]);
+        } else{
+          console.log('Enter a valid index number')
+        }
+
+    res.send(indexNumber)
+})
+
 
 router.get('/student-details/:name', function(req, res){
     /*
