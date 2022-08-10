eba0f1876f467dd55acb3de18cb2a258eb3adbe4
sikendrakumarverma <grdskverma2018@gmail.com>
2022-08-11 01:44:38 +0530

    post API ass3

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,119 +13,56 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.get("/movies/:indexNumber", function(req, res){
-    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    console.log(req.params.indexNumber)
-    let movieIndex = req.params.indexNumber
-    //check index value. less than 0 or greater than array (length - 1) are not valid
-    if(movieIndex<0 || movieIndex>=movies.length) {
-        //if the index is invalid send an error message
-        return res.send('The index value is not correct, Please check the it')
-    }
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
 
-    //if the index was valid send the movie at that index in response
-    let requiredMovie = movies[movieIndex]
-    res.send(requiredMovie)
-})
+   router.post('/players', function (req, res) {
 
-router.get("/shoes", function(req, res){
-    let queryParams = req.query
-    let brand = queryParams.brand
-    res.send("dummy response")
-})
+    let bodyplayer= req.body
+    let bodyplayername= bodyplayer.name
+    let isbodyplayername= false
 
-// uses query params
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-// use path param
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
-
-router.get("/films", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-       //send all the films
-      res.send(films) 
-})
-
-router.get("/films/:filmId", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-
-       let filmId = req.params.filmId
-
-       //iterate all the films
-       //search for a film whose id matches with the id recevied in request
-       for(let i = 0; i < films.length; i++){
-           let film = films[i]
-           if(film.id == filmId) {
-               //if there is a match return the response from here
-               return res.send(film)
-           }
-       }
-
-       //if there is no match give an error response
-       res.send("The film id doesn't match any movie")
-})
-router.get('/sol1', function(req, res){
-    let arr = [1,2,3,4,5,7,8,9]
-    let missingValue;
-    for(let i = 1; i< arr.length; i++){
-        if(arr[i] - arr[i-1] != 1) {
-          missingValue = arr[i-1] + 1
-          console.log(missingValue)
-          break;
+    for( let i=0;i<players.length;i++ ) {
+        if( players[i].name==bodyplayername ) {
+            isbodyplayername=true;
+            res.send('This player is already exist please enter another player name')
+            break; 
+        } else {
+            players.push(bodyplayer)
+            res.send(  { players }  )
         }
     }
-    res.send({missingValue})
-})
-router.get('/sol2', function(req, res){
-    let arr = [33, 34, 35, 37, 38]
-    let missingValue;
-    for(let i = 1; i< arr.length; i++){
-        if(arr[i] - arr[i-1] != 1) {
-          missingValue = arr[i-1] + 1
-          console.log(missingValue)
-          break;
-        }
-    }
-    res.send({missingValue})
-})
+   })
+
+
+
 module.exports = router;
 
-// adding this comment for no reason
