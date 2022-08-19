f1590e9e4db515001aa41a084a9a70efbcbf0d47
sikendrakumarverma <grdskverma2018@gmail.com>
2022-08-19 16:25:32 +0530

    classnotes of 19 AUG

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -14,4 +14,4 @@ const bookSchema = new mongoose.Schema( {
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('Book', bookSchema)
